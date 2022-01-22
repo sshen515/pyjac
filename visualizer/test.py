80699c42dbb2bf323370cf80dc55a9412eb87889
Yiqi Shen <scottshen0515@gmail.com>
2022-01-22 10:45:46 -0500

    Added activities to start python script; updated script to use relative paths

--- visualizer/test.py
+++ visualizer/test.py
@@ -4,7 +4,7 @@ from plotly.subplots import make_subplots
 import plotly.graph_objects as go
 
 # pie chart
-with open("C:/UTM/Winter2022/pyjac/transcript/tmp/tmp.txt") as file:
+with open("../transcript/tmp/tmp.txt") as file:
     lines = file.readlines()
     lines = [line.rstrip() for line in lines]
     grades_list = []
@@ -27,5 +27,5 @@ fig.add_trace(go.Pie(labels=list(courses.keys()), values=list(courses.values()),
               1, 1)
 
 fig.update_layout(height=600, width=800, title_text="CourseWise Breakdown")
-fig.write_html("C:/UTM/Winter2022/test/file.html")
+fig.write_html("../visualizer/file.html")
         
